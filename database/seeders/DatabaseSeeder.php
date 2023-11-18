33b44acc466c7f1c964a51966a46620db4d59c7f
Mitsuka325 <mitsumitsutsu@gmail.com>
2023-11-18 10:53:08 +0900

    学年seeder作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Grade;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +22,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Grade::create(['grade_name' => '小学1年生']);
+        Grade::create(['grade_name' => '小学2年生']);
+        Grade::create(['grade_name' => '小学3年生']);
+        Grade::create(['grade_name' => '小学4年生']);
+        Grade::create(['grade_name' => '小学5年生']);
+        Grade::create(['grade_name' => '小学6年生']);
+        Grade::create(['grade_name' => '中学1年生']);
+        Grade::create(['grade_name' => '中学2年生']);
+        Grade::create(['grade_name' => '中学3年生']);
+        Grade::create(['grade_name' => '高校1年生']);
+        Grade::create(['grade_name' => '高校2年生']);
+        Grade::create(['grade_name' => '高校3年生']);
     }
 }
